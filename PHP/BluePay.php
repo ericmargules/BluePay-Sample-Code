d5e02157230515b9cf318c455895939f0ea71fa3
Eric Margules <emargules@bluepay.com>
2018-12-06 15:13:05 -0600

    get substring of PHP location header

--- PHP/BluePay.php
+++ PHP/BluePay.php
@@ -928,7 +928,7 @@ class BluePay {
                 $headers = explode("\n", $headers);
                 foreach($headers as $header) {
                     if (stripos($header, 'Location:') !== false) {
-                        $this->response = $header;
+                        $this->response = substr($header, strpos($header, "?")+ 1);
                     }
                 }
             } else {
